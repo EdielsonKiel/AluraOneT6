02238dd35278e120bee527e49f0a472b7c0342e4
Edielson Kiel <edielson.kiel.dev@gmail.com>
2024-01-17 16:00:53 -0300

    primeira interação com java script e uso de alertas

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AluraOneT6
+
+17/01/2014 começo de logica de programação e java script
---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,1 @@
+alert('Bem Vindo ao jogo de adivinhação de numero'); // 17/01/2024 para o java script exibir uma mensagem dentro de um alerta o texto deve estar entre aspas simples ou duplas
